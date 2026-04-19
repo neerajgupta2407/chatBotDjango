050675537735f4f18744032ab7c99eac09574e48
neeraj <neerajgupta2407@gmail.com>
2026-04-19 11:31:50 +0530

    Make base settings read DATABASE_URL so default fallback no longer forces SQLite

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,12 +100,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db(default="sqlite:///" + str(BASE_DIR / "db.sqlite3"))}
 
 
 # Password validation
